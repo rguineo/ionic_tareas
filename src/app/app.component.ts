8b3a26a8222a8b910fb0e7b2164541a5683dbe53
Roberto Guineo <rguineoa@gmail.com>
2022-12-05 21:38:09 -0400

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,15 @@ import { DatabaseService } from './services/database.service';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
   public appPages = [
     { title: 'Nueva Tarea', url: '/new-task', icon: 'add-circle' },
-    { title: 'Categorias', url: '/categories', icon: 'paper-plane' },
-    { title: 'Tareas', url: '/tasks', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Categorias', url: '/categories', icon: 'bookmarks' },
+    { title: 'Tareas', url: '/tasks', icon: 'calendar' },
+    // { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+    // { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    // { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
